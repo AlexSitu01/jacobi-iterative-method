6672ab6c0ab6babba3c36bc14b71d763a707b568
Alex <situalex123@gmail.com>
2023-10-24 22:52:40 -0700

    final commit added console inputs

--- src/main.java
+++ src/main.java
@@ -1,17 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        double[][] matrix = {{5, -1, 0, 7},
-                             {-1, 3, -1, 4},
-                             {0, -1, 2 , 5}};
+        Scanner scnr = new Scanner(System.in);
+        scnr.useLocale(Locale.US);
+        System.out.println("How many linear equations do you want to solve for?: ");
+        int numEquations = scnr.nextInt();
+        scnr.nextLine();
+        double[][] matrix = new double[numEquations][numEquations+1];
+
+        System.out.println("How do you want to enter the matrix \n1. Enter matrix manually \n2. Read from file");
+        int userChoice = scnr.nextInt();
+        scnr.nextLine();
+
+
+        if(userChoice == 1){
+            populateMatrixFromUserInput(matrix, scnr);
+        }
+        else if(userChoice == 2 ){
+            //creating matrix
+            boolean notDone = true;
+            while(notDone){
+                try{populateMatrixFromFile(matrix, scnr);
+                    notDone = false;
+                }
+                catch(FileNotFoundException exception){
+                    System.out.println("The file inputted can't be found.");
+                    System.out.println(exception);
+                }
+            }
+        }
+        else{
+            System.out.println("Invalid option");
+            return;
+        }
+
+
+        System.out.println("Enter error tolerance: ");
+        double precision = scnr.nextDouble();
+        scnr.nextLine();
+
+
+
+        System.out.println("Enter starting x solutions: ");
+        String[] str = scnr.nextLine().split(" ");
+        double[] startingX = new double[matrix.length];
+        for (int i = 0; i < str.length; i++){
+            startingX[i] = Double.parseDouble(str[i]);
+        }
+
+        double[] copyStartingX = Arrays.copyOf(startingX, startingX.length);
+        if (checkConvergence(matrix)){
+            System.out.println("The matrix is strictly diagonally dominant and therefore both the Jacobi and Gauss-Seidel methods will converge to an answer.");
+        }
+        else{
+            System.out.println("The matrix is not strictly diagonally dominant, so the Jacobi and Gauss-Seidel methods might not converge to an answer.");
+        }
         System.out.println(checkConvergence(matrix));
-        double[] initX = {0,0,0};
-        guass_seidel(matrix, initX, 0);
-        initX = new double[]{0, 0, 0};
-        jacobi(matrix,initX, 0);
+        guass_seidel(matrix, startingX, precision);
+        jacobi(matrix,copyStartingX, precision);
 
     }
+
+    public static void populateMatrixFromFile(double[][] matrix, Scanner scnr) throws FileNotFoundException {
+        System.out.println("Enter file path: ");
+        Scanner fileScnr = new Scanner(new File(scnr.nextLine()));
+
+        for (double[] row : matrix) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = fileScnr.nextFloat();
+            }
+        }
+        fileScnr.close();
+    }
+    public static void populateMatrixFromUserInput(double[][] matrix, Scanner scnr) {
+        for (int row = 0; row < matrix.length; row++) {
+            System.out.println("Enter row " + row + "'s values: ");
+            String[] values = scnr.nextLine().split(" ");
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = Double.parseDouble(values[column]);
+            }
+        }
+    }
+
     public static void printArray (double[] array){
 
         System.out.print("[");
@@ -26,7 +102,7 @@ public class main {
 
     public static double[] guass_seidel (double[][] matrix, double[] xVal ,double precision){
         System.out.println();
-        System.out.println("Guass Sediel Method: ");
+        System.out.println("Guass-Sediel Method: ");
         System.out.println();
         int iteration = 0;
         int numColumns = matrix[0].length-1;
